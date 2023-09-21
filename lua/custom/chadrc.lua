0663bb087f2d34f7d222966289244d1e1de1ecb6
Tyler Schock <schock3736@gmail.com>
2023-09-20 19:38:51 -0500

    Add statusline config

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,7 +1,12 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'gruvbox' }
+M.ui = {
+  statusline = {
+    theme = "default"
+  },
+  theme = 'gruvbox' 
+}
 M.plugins = 'custom.plugins'
 M.mappings = require 'custom.mappings'
 return M
