5aa8495f25bff2c89b201c5450ad4c86c5c13e62
Tyler Schock <schock3736@gmail.com>
2024-01-12 17:14:04 -0600

    set default jdk for nvim-java

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,51 +1,72 @@
-local configs = require("plugins.configs.lspconfig")
+local configs = require "plugins.configs.lspconfig"
 local on_attach = configs.on_attach
 local capabilities = configs.capabilities
-local java = require('java')
+local java = require "java"
 local lspconfig = require "lspconfig"
 
 java.setup()
 
-lspconfig.docker_compose_language_service.setup{
+lspconfig.docker_compose_language_service.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = { "yaml.docker-compose", "yml.docker-compose" }
+  filetypes = { "yaml.docker-compose", "yml.docker-compose" },
 }
 
-lspconfig.dockerls.setup{
+lspconfig.dockerls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = { "dockerfile" }
+  filetypes = { "dockerfile" },
 }
 
 lspconfig.jdtls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = { "java" }
+  filetypes = { "java" },
+  settings = {
+    java = {
+      configuration = {
+        runtimes = {
+          {
+            name = "JavaSE-21",
+            path = "~/.sdkman/candidates/java/21.0.1-graal/",
+            default = true,
+          },
+        },
+      },
+    },
+  },
 }
 
-lspconfig.marksman.setup{
+lspconfig.marksman.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = { "markdown" }
+  filetypes = { "markdown" },
 }
 
-
-lspconfig.pyright.setup{
+lspconfig.pyright.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = { "python" }
+  filetypes = { "python" },
 }
 
-lspconfig.svelte.setup{
+lspconfig.svelte.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = { "svelte" }
+  filetypes = { "svelte" },
 }
 
-lspconfig.volar.setup{
+lspconfig.volar.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = { "typescript", "javascript", "javascriptreact", "javascriptreact.jsx", "typescriptreact", "typescriptreact.tsx", "vue", "json", "css" },
+  filetypes = {
+    "typescript",
+    "javascript",
+    "javascriptreact",
+    "javascriptreact.jsx",
+    "typescriptreact",
+    "typescriptreact.tsx",
+    "vue",
+    "json",
+    "css",
+  },
 }
-
